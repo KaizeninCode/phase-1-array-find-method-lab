dffb3d2dc6e8f7642713df304549a8709c82afd8
Phil Wekullo <philwekullo@gmail.com>
2023-12-20 08:32:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = []
+function superbowlWin(record) {
+    for(let win of record){
+        if (win.result === 'W'){
+            return win.year
+        }
+    }
+}
+
+superbowlWin(record)
